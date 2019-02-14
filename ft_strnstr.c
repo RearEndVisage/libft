d913fb9e1e86ddb6991f14d4c1209dc0bc388ef5
cmckelvy <cmckelvy@42.US.ORG>
2019-02-14 15:43:13 -0800

    part 1???

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,28 +6,28 @@
 /*   By: cmckelvy <cmckelvy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/13 18:50:26 by cmckelvy          #+#    #+#             */
-/*   Updated: 2019/02/14 15:41:56 by cmckelvy         ###   ########.fr       */
+/*   Updated: 2019/02/14 15:43:07 by cmckelvy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *str, const char *to_find, size_t len)
 {
 	size_t	i;
-	size_t	n;
+	size_t	k;
 
-	if (!needle[0])
-		return ((char*)haystack);
+	if (!to_find[0])
+		return ((char*)str);
 	i = -1;
-	while (haystack[++i])
+	while (str[++i])
 	{
-		n = 0;
-		while (needle[n] == haystack[i + n] && i + n < len)
-			if (!needle[n++])
-				return ((char*)&haystack[i]);
-		if (n != 0 && !needle[n])
-			return ((char*)&haystack[i]);
+		k = 0;
+		while (to_find[k] == str[i + k] && i + k < len)
+			if (!to_find[k++])
+				return ((char*)&str[i]);
+		if (k != 0 && !to_find[k])
+			return ((char*)&str[i]);
 	}
 	return (NULL);
 }
